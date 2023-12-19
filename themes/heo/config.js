f68401446423f74bcf1da2c64697d5c329a9ba33
owenwei90 <42132073+wzf19901221666@users.noreply.github.com>
2023-12-19 21:10:52 +0800

    Update config.js

--- themes/heo/config.js
+++ themes/heo/config.js
@@ -18,7 +18,7 @@ const CONFIG = {
   HEO_HERO_TITLE_LINK: 'https://lintyco.top',
 
   // 英雄区显示三个置顶分类
-  HEO_HERO_CATEGORY_1: { title: '必看精选', url: '/tag/必看精选' },
+  HEO_HERO_CATEGORY_1: { title: '必看精选', url: '/tag/推荐' },
   HEO_HERO_CATEGORY_2: { title: '热门文章', url: '/tag/热门文章' },
   HEO_HERO_CATEGORY_3: { title: '实用教程', url: '/tag/实用教程' },
 
@@ -103,7 +103,7 @@ const CONFIG = {
   HEO_SOCIAL_CARD_TITLE_1: '交流频道',
   HEO_SOCIAL_CARD_TITLE_2: '加入我们的社群讨论分享',
   HEO_SOCIAL_CARD_TITLE_3: '点击加入社群',
-  HEO_SOCIAL_CARD_URL: 'https://docs.tangly1024.com/article/how-to-question',
+  HEO_SOCIAL_CARD_URL: 'https://lintyco.top/shequn',
 
   // *****  以下配置无效，只是预留开发 ****
   // 菜单配置
